ab130d762d86c36a59be12f726828c0404c62808
pokutnev <pokutnev02@gmail.com>
2025-01-17 19:54:40 +0300

    init commit

---
+++ src/main/java/romashkaco/moitovary/MoitovaryApplication.java
@@ -0,0 +1,13 @@
+package romashkaco.moitovary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoitovaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoitovaryApplication.class, args);
+	}
+
+}
